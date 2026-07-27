a776ff31255669bf0c123183866c853b4930148e
marchelrn <marchelmanullang026@student.unsrat.ac.id>
2026-07-27 11:58:27 +0800

    feat(models): change ID types from string into int

--- models/user.go
+++ models/user.go
@@ -9,11 +9,11 @@ const (
 
 // User represents the users table.
 type User struct {
-	ID        string `json:"id" gorm:"column:id;type:uuid;default:gen_random_uuid();primaryKey"`
-	Name      string `json:"name" gorm:"column:name;not null"`
-	Email     string `json:"email" gorm:"column:email;not null;uniqueIndex"`
-	Password  string `json:"-" gorm:"column:password;not null"`
-	Role      string `json:"role" gorm:"column:role;not null;default:operator"`
+	ID        int       `json:"id" gorm:"column:id;primaryKey;autoIncrement"`
+	Name      string    `json:"name" gorm:"column:name;not null"`
+	Email     string    `json:"email" gorm:"column:email;not null;uniqueIndex"`
+	Password  string    `json:"-" gorm:"column:password;not null"`
+	Role      string    `json:"role" gorm:"column:role;not null;default:operator"`
 	CreatedAt time.Time `json:"created_at" gorm:"column:created_at;autoCreateTime"`
 }
 
